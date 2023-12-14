adf7f2998be6c32b00ee0263c113f17b76e4886c
vygantaspaul <wygantas.pauliukonis@gmail.com>
2023-12-14 12:41:15 +0200

    styles

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Location.module.css";
+
 type LocationType = {
   _id: string;
   title: string;
@@ -7,20 +8,44 @@ type LocationType = {
   longitude: number;
   latitude: number;
   location_photo_url: string;
-  location_id: string;
 };
+
 type LocationComponentType = {
   location: LocationType;
 };
+
 const Location: React.FC<LocationComponentType> = ({ location }) => {
   return (
     <a
-      className={`bg-slate-800 hover:bg-slate-500 max-h-80 block rounded-lg object-cover px-6 py-20 ring-1 text-white ring-slate-900/5 shadow-xl ${styles.custom}`}
+      className={`bg-slate-800 hover:bg-slate-500 max-h-80 block rounded-lg relative overflow-hidden`}
       href={`location/${location._id}`}
     >
-      <div>Title: {location.title}</div>
-      <div>longitude: {location.longitude}</div>
-      <div>latitude: {location.latitude}</div>
+      {location.location_photo_url ? (
+        <>
+          <img
+            src={location.location_photo_url}
+            alt={location.title}
+            className="object-cover w-full h-full"
+          />
+          <div className="absolute inset-0 px-6 py-20 text-white bg-slate-400/50">
+            <div>Title: {location.title}</div>
+            <div>longitude: {location.longitude}</div>
+            <div>latitude: {location.latitude}</div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className=" w-full h-80  flex  bg-slate-400/50 text-white">
+            <div className=" flex items-center  px-6  ">
+              <div>
+                <div>Title: {location.title}</div>
+                <div>longitude: {location.longitude}</div>
+                <div>latitude: {location.latitude}</div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </a>
   );
 };
